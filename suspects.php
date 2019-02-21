16f72f4c84f9a985775474f6691f21a3a0535e1f
01cmwamidi <01cmwamidi@gmail.com>
2019-02-21 12:03:24 +0300

    Displayed List Of Suspects

--- suspects.php
+++ suspects.php
@@ -23,6 +23,48 @@ require 'security.php';
 
 <?php require 'navbar.php'; ?>
 
+<div class="container">
+    <table class="table ">
+        <thead>
+        <tr>
+            <td>ID</td>
+            <td>NAMES</td>
+            <td>IDENTITY</td>
+            <td>GENDER</td>
+            <td>DATE IN</td>
+            <td>TYPE</td>
+        </tr>
+
+        </thead>
+
+        <tbody>
+
+        <?php
+        require 'db.php';
+        $sql = "SELECT * FROM suspects";
+        $results = mysqli_query($conn,$sql);
+        while($row =mysqli_fetch_assoc($results))
+        {
+         extract($row);
+         echo "<tr>
+            <td>$id</td>
+            <td>$names</td>
+            <td>$identity</td>
+            <td>$gender</td>
+            <td>$date</td>
+            <td>$type</td>
+        </tr>";
+        }
+        ?>
+
+        </tbody>
+
+    </table>
+</div>
 
 </body>
 </html>
+<?php
+//git add.
+//git commit -m "Displayed List Of Suspects"
+//git push
